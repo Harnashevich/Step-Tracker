dfb00bb59a3d3254615853412a4c109ac15efbda
Andrey Garnoshevich <garnoshevich@alkor.co.ru>
2025-07-29 13:20:06 +0300

    StepPieChart animation

--- Step Tracker/Charts/StepPieChart.swift
+++ Step Tracker/Charts/StepPieChart.swift
@@ -11,6 +11,16 @@ import Charts
 struct StepPieChart: View {
     
     @State private var rawSelectedChartValue: Double?
+    
+    var selectedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+        var total = 0.0
+        
+        return chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+    }
 
     var chartData: [WeekdayChartData]
 
@@ -32,19 +42,38 @@ struct StepPieChart: View {
                     SectorMark(
                         angle: .value("Average Steps", weekday.value),
                         innerRadius: .ratio(0.6),
+                        outerRadius: selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
                         angularInset: 1)
                     .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
+                    .opacity(selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1 : 0.3)
                 }
             }
-            .chartAngleSelection(value: $rawSelectedChartValue)
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        let frame = geo[plotFrame]
+                        if let selectedWeekday {
+                            VStack {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                    .contentTransition(.opacity)
+                                
+                                Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: frame.midY)
+                        }
+                    }
+                }
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
-        .onChange(of: rawSelectedChartValue) { oldValue, newValue in
-            print(newValue)
-        }
     }
 }
 
