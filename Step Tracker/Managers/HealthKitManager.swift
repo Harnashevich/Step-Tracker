c34a5276b294da291d2310c6c858fad36f040a55
Andrey Garnoshevich <garnoshevich@alkor.co.ru>
2025-08-11 17:39:46 +0300

    add validation

--- Step Tracker/Managers/HealthKitManager.swift
+++ Step Tracker/Managers/HealthKitManager.swift
@@ -14,7 +14,8 @@ enum STError: LocalizedError {
     case sharingDenied(quantityType: String)
     case noData
     case unableToCompleteRequest
-    
+    case invalidValue
+
     var errorDescription: String? {
         switch self {
         case .authNotDetermined:
@@ -25,9 +26,11 @@ enum STError: LocalizedError {
             "No Data"
         case .unableToCompleteRequest:
             "Unable to Complete Request"
+        case .invalidValue:
+            "Invalid Value"
         }
     }
-    
+
     var failureReason: String {
         switch self {
         case .authNotDetermined:
@@ -38,6 +41,8 @@ enum STError: LocalizedError {
             "There is no data for this Health statistic."
         case .unableToCompleteRequest:
             "We are unable to complete your request at this time.\n\nPlease try again later or contact support."
+        case .invalidValue:
+            "Must be a numeric value with a maximum of one decimal place."
         }
     }
 }
